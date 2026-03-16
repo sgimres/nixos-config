86077ba140927405bd83c947bcb72f7dd53aaab7
sgimres <148025215+deepattic@users.noreply.github.com>
2026-03-16 16:49:35 +0000

    rm unwanted console term..

--- configuration.nix
+++ configuration.nix
@@ -35,11 +35,11 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-    useXkbConfig = true; # use xkb.options in tty.
-  };
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
