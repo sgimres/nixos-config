3127b17f4775bdc953213c7aeaf76032dce8e932
deepattic <148025215+deepattic@users.noreply.github.com>
2026-03-16 22:15:36 +0530

    simplyfy fail2ban

--- modules/fail2ban.nix
+++ modules/fail2ban.nix
@@ -4,13 +4,10 @@
   services.fail2ban = {
     enable = true;
     maxretry = 5;
-    bantime = "24h"; # Ban IPs for one day on the first ban
+    bantime = "24h";
     bantime-increment = {
-      enable = true; # Enable increment of bantime after each violation
-      formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
-      multipliers = "1 2 4 8 16 32 64";
-      maxtime = "1y";
-      overalljails = true; # Calculate the bantime based on all the violations
+      enable = true;
+      overalljails = true;
     };
   };
 }
